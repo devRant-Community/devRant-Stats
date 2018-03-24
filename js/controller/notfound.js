f9e131b2f731eef47ae2b59e24fd4546348850de
Skayo <jonasg.cool@gmail.com>
2018-03-24 13:29:50 +0100

    Test if user exists when requesting add

--- js/controller/notfound.js
+++ js/controller/notfound.js
@@ -2,11 +2,21 @@ app.controller('NotFoundController', function ($scope, $location, $http, $routeP
 	$scope.headerBackground = "d55161";
 
 	angular.element(document).ready(function () {
-		setTimeout(function (){
+		setTimeout(function () {
 			var addUser = confirm("Do you want to send a request for the user to be added?");
 
-			if(addUser && $routeParams.username !== undefined) {
-				$http.get('https://skayo.2ix.de/api/requestAddUser.php?username=' + $routeParams.username).then(function() {
+			if (addUser && $routeParams.username !== undefined) {
+				$http.get('https://devrant.com/api/get-user-id?app=3&username=' + $routeParams.username).then(function(response) {
+					if(response.data.success) {
+						$http.get('https://skayo.2ix.de/api/requestAddUser.php?username=' + $routeParams.username).then(function () {
+							$location.path("/");
+						}, function() {
+							$location.path("/");
+						});
+					} else {
+						$location.path("/");
+					}
+				}, function() {
 					$location.path("/");
 				});
 			} else {
