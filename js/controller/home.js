76adedd68aaf434a9260f91d9379071c72cadc46
Skayo <jonasg.cool@gmail.com>
2018-03-25 17:12:41 +0200

    Added Autocomplete

--- js/controller/home.js
+++ js/controller/home.js
@@ -10,7 +10,11 @@ app.controller('HomeController', function ($scope, $location, $http) {
 		incomeYear: "Loading...",
 		scoreSum: "Loading...",
 		rantsSum: "Loading..."
-	}
+	};
+
+	$scope.usernames = [];
+	$scope.usernameAutocomplete = {post: "", pre: "", full: ""};
+	$scope.autocompleteHidden = true;
 
 	$http.get('https://skayo.2ix.de/api/getHomepageStats.php').then(function (response) {
 		if(response.data.success) {
@@ -27,13 +31,32 @@ app.controller('HomeController', function ($scope, $location, $http) {
 		}
 	});
 
+	$http.get('https://skayo.2ix.de/api/getAllUsernames.php').then(function (response) {
+		$scope.usernames = response.data.usernames;
+	});
+
 	$scope.startSearch = function() {
 		$location.path('/user/' + $scope.search);
 	};
 
-	$scope.onSearchEnter = function(event) {
-		if(event.which === 13) {
-			$scope.startSearch();
+	$scope.startSearchAutocomplete = function() {
+		$location.path('/user/' + $scope.search + $scope.usernameAutocomplete.post);
+	};
+
+	$scope.searchUsername = function() {
+		if($scope.search === '') return;
+
+		var result = $scope.usernames.find(function(username) {
+			return username.toLowerCase().startsWith($scope.search.toLowerCase());
+		});
+
+		if(result !== undefined) {
+			$scope.usernameAutocomplete.full = result;
+			$scope.usernameAutocomplete.pre = $scope.search;
+			$scope.usernameAutocomplete.post = result.replace(new RegExp('^(' + $scope.search + ')', 'gi'), '');
+			$scope.autocompleteHidden = false;
+		} else {
+			$scope.autocompleteHidden = true;
 		}
-	}
+	};
 });
